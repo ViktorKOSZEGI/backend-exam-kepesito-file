cb6159ca85ba4df46c7082ecf59c41ce7ede6f97
ViktorKOSZEGI <koszegiv.5@gmail.com>
2021-04-29 11:57:12 +0200

    Implement exportPhonebook() method

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,25 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if ("null".equals(contacts) || "null".equals(String.valueOf(output))) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        Path filePath = Path.of(output);
+        try (BufferedWriter bw = Files.newBufferedWriter(filePath, StandardOpenOption.APPEND)) {
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                String line = entry.getKey() + ": " + entry.getValue() + "\n";
+                bw.write(line);
+            }
+        } catch (Exception throwables) {
+            throw new IllegalArgumentException("Exception occured");
+        }
+    }
+}
